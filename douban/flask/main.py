b9230599af0222a3ef35dbfd63ee5776ee50ba2b
boredcui <1637188453@qq.com>
2022-05-16 20:51:05 +0800

    day 6

--- douban/flask/main.py
+++ douban/flask/main.py
@@ -4,13 +4,14 @@
 Author: boredcui 1637188453@qq.com
 Date: 2022-05-12 15:11:16
 LastEditors: boredcui 1637188453@qq.com
-LastEditTime: 2022-05-13 15:09:26
+LastEditTime: 2022-05-13 21:29:59
 FilePath: \flask\main.py
 Description: 
 
 Copyright (c) 2022 by boredcui 1637188453@qq.com, All Rights Reserved. 
 '''
 from flask import Flask,render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -19,6 +20,37 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/index")
+def index():
+    # return render_template("index.html")
+    return hello()
+
+
+@app.route("/movie")
+def movie():
+    return render_template("movie.html")
+
+
+@app.route("/music")
+def music():
+    return render_template("music.html")
+
+
+@app.route("/book")
+def book():
+    return render_template("book.html")
+
+
+@app.route("/score")
+def score():
+    return render_template("score.html")
+
+
+@app.route("/word")
+def word():
+    return render_template("word.html")
 
 
 if __name__ == '__main__':
